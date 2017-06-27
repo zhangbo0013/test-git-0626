5c43ac167b54a665c233c647a7f96828aca350b5
zhangbo0013 <2748482021@qq.com>
2017-06-27 22:54:04 +0800

    更改02

--- demo0626/app/src/main/java/com/zhang/demo0626/MainActivity.java
+++ demo0626/app/src/main/java/com/zhang/demo0626/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //更改01
+        //更改02
     }
 }
